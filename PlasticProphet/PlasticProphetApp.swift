bb8570f30382dc44cb0c0593841d9de750fdf3b4
JaRappa <51486940+JaRappa@users.noreply.github.com>
2025-10-06 11:47:45 -0400

    Add files via upload

---
+++ PlasticProphet/PlasticProphetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlasticProphetApp.swift
+//  PlasticProphet
+//
+//  Created by Jake on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PlasticProphetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
